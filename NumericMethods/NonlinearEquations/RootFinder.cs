4b05df2884d0363a40d8d744405d06c177b1c150
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-09-04 23:56:01 +0300

    Add nl equations task project

---
+++ NumericMethods/NonlinearEquations/RootFinder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonlinearEquations
+{
+    class RootFinder
+    {
+        private Func<double, double> function;
+        public RootFinder(Func<double, double> function)
+        {
+            this.function = function;
+        }
+
+        public static List<(double, double)> Separate((double start, double end) interval, int stepNumber)
+        {
+            if (interval.start >= interval.end)
+            {
+                throw new RootFinderException("Отделение корней", "Начало исходного интервала должно быть меньше конца");
+            }
+
+            return null;
+        }
+
+        public static IntervalResult Bisection((double start, double end) interval)
+        {
+            return null;
+        }
+
+        public static IntervalResult Newtons((double start, double end) interval)
+        {
+            return null;
+        }
+
+        public static IntervalResult NewtonsModified((double, double) interval)
+        {
+            return null;
+        }
+
+        public static IntervalResult Secant((double, double) interval)
+        {
+            return null;
+        }
+    }
+}
---
+++ NumericMethods/NonlinearEquations/RootFinderException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonlinearEquations
+{
+    public class RootFinderException : Exception
+    {
+        public RootFinderException(string operation, string message) : base($"{operation}: {message}") { }
+    }
+}
