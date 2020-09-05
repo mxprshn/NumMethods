db5322708576f5b7e539c9e67a1ca8ad0c83c8e5
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-09-05 10:39:43 +0300

    Started separation

---
+++ NumericMethods/NonlinearEquations/DoubleExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NonlinearEquations
+{
+    static class DoubleExtensions
+    {
+        public static bool RoughlyEquals(this double first, double second) =>
+            Math.Abs(first - second) < 0.00001;
+    }
+}
--- NumericMethods/NonlinearEquations/RootFinder.cs
+++ NumericMethods/NonlinearEquations/RootFinder.cs
@@ -12,32 +12,73 @@ namespace NonlinearEquations
             this.function = function;
         }
 
-        public static List<(double, double)> Separate((double start, double end) interval, int stepNumber)
+        public List<(double, double)> Separate((double start, double end) interval, int stepNumber)
         {
+            // Check interval ends for being finite
+
+
             if (interval.start >= interval.end)
             {
                 throw new RootFinderException("Отделение корней", "Начало исходного интервала должно быть меньше конца");
             }
 
+            if (stepNumber < 2)
+            {
+                throw new RootFinderException("Отделение корней", "Число шагов должно быть больше 1");
+            }
+
+            var result = new List<(double, double)>();
+
+            var stepLength = (interval.end - interval.start) / stepNumber;
+
+            var x1 = interval.start;
+            var x2 = x1 + stepLength;
+
+            var y1 = function(x1);
+            var y2 = function(x2);
+
+            for (var i = 1; i <= stepNumber; ++i)
+            {
+                if (i == stepNumber)
+                {
+                    x2 = interval.end;
+                    y2 = function(interval.end);
+                }
+
+                // Check for zero separately.
+
+                if (y1 * y2 <= 0)
+                {
+                    result.Add((x1, x2));
+                }
+
+                
+            }
+
+            while (!x2.RoughlyEquals(interval.end))
+            {
+
+            }
+
             return null;
         }
 
-        public static IntervalResult Bisection((double start, double end) interval)
+        public IntervalResult Bisection((double start, double end) interval)
         {
             return null;
         }
 
-        public static IntervalResult Newtons((double start, double end) interval)
+        public IntervalResult Newtons((double start, double end) interval)
         {
             return null;
         }
 
-        public static IntervalResult NewtonsModified((double, double) interval)
+        public IntervalResult NewtonsModified((double, double) interval)
         {
             return null;
         }
 
-        public static IntervalResult Secant((double, double) interval)
+        public IntervalResult Secant((double, double) interval)
         {
             return null;
         }
