0c097749a51e7399eccadc8e63edbab459778afa
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-09-19 10:25:49 +0300

    Add lagrange interpolation

--- lagrange/main.py
+++ lagrange/main.py
@@ -3,7 +3,7 @@ import math
 
 
 def function(x):
-    return 2 * x  # math.cos(x) + 2 * x
+    return x ** 2 + 7 * (x ** 4) + 4 * (x ** 6)  # math.cos(x) + 2 * x
 
 
 separator = "________________________________________________________"
@@ -61,14 +61,59 @@ if __name__ == '__main__':
         except ValueError:
             print_error("Некорректное значение: введите вещественное число, большее чем начало отрезка")
 
-    valueTable = {}
-    segmentLength = (end - start) / numberOfValues
+    valueTable = []
+    segmentLength = (end - start) / (numberOfValues - 1)
 
     for i in range(numberOfValues):
         x = start + segmentLength * i
-        valueTable[x] = function(x)
+        valueTable.append((x, function(x)))
 
-    print("Таблица значений функции:\n")
+    print("Таблица значений функции:")
 
-    for x, y in valueTable.items():
+    for x, y in valueTable:
         print(float_to_string(x) + " | " + float_to_string(y))
+
+    interpolationPointX = 0.0
+
+    while True:
+        try:
+            interpolationPointX = float(input("Введите точку интерполяции: "))
+            break
+        except ValueError:
+            print_error("Некорректное значение: введите вещественное число")
+
+            interpolationPointX = 0.0
+
+    interpolationDegree = 0
+
+    while True:
+        try:
+            interpolationDegree = int(input(f"Введите степень интерполяционного многочлена (< {numberOfValues}): "))
+            if interpolationDegree < 1 or interpolationDegree >= numberOfValues:
+                raise ValueError
+            break
+        except ValueError:
+            print_error("Некорректное значение: введите целое число, большее нуля и меньшее числа значений в таблице "
+                        "функции")
+
+    valueTable.sort(key=lambda xy: abs(xy[0] - interpolationPointX))
+    interpolationNodes = valueTable[:interpolationDegree + 1]
+
+    print("Узлы интерполяции:")
+
+    for x, y in interpolationNodes:
+        print(float_to_string(x) + " | " + float_to_string(y))
+
+    print(f"""
+Точка интерполирования: {float_to_string(interpolationPointX)}
+Степень интерполяционного многочлена: {interpolationDegree}
+
+{separator}
+    """)
+
+    interpolation_point_y = lagrange.interpolate(interpolationNodes, interpolationPointX)
+    print(f"Значение интерполяционного многочлена в точке {float_to_string(interpolationPointX)}"
+          f": {float_to_string(interpolation_point_y)}")
+
+    value_error = abs(function(interpolationPointX) - interpolation_point_y)
+    print(f"Абсолютная фактическая погрешность: {float_to_string(value_error)}")
