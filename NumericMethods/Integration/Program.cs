ba660deefe4d01628c06d1ea4431ade72229df76
Max Parshin <maksim.parshin@lanit-tercom.com>
2020-11-05 10:43:34 +0300

    Add integration

---
+++ NumericMethods/Integration/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NumAn;
+
+namespace Integration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Лабораторная работа #4\n\n" +
+                "Приближённое вычисление интеграла по составным квадратурным формулам\n\n");
+
+            while (Integration()) { };
+        }
+
+        private static bool Integration()
+        {
+            Console.Write("Введите левый конец отрезка интегрирования: ");
+
+            var left = 0d;
+
+            while (!double.TryParse(Console.ReadLine(), out left) || !left.IsNumber())
+            {
+                Console.Write("Некорректное значение: введите вещественное число: ");
+            }
+
+            Console.Write("Введите правый конец отрезка интегрирования: ");
+
+            var right = 0d;
+
+            while (!double.TryParse(Console.ReadLine(), out right) || !right.IsNumber() || right <= left)
+            {
+                Console.Write("Некорректное значение: введите вещественное число, большее левого конца отрезка интегрирования: ");
+            }
+
+            Console.Write("Введите число промежутков деления отрезка интегрирования: ");
+
+            var segmentNumber = 0;
+
+            while (!int.TryParse(Console.ReadLine(), out segmentNumber) || segmentNumber <= 0)
+            {
+                Console.Write("Некорректное значение: введите положительное целое число: ");
+            }
+
+
+
+            Console.WriteLine($"\nЧтобы выйти, нажмите \'Esc\'\n");
+
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ NumericMethods/NumAn/Integrator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumAn
+{
+    public static class Integrator
+    {
+        public class Integral
+        {
+            public Integral(double leftRecangle, double rightRecangle, double middleRectangle, double trapezoidal, double simpsons)
+            {
+                LeftRecangle = leftRecangle;
+                RightRecangle = rightRecangle;
+                MiddleRecangle = middleRectangle;
+                Trapezoidal = trapezoidal;
+                Simpsons = simpsons;
+            }
+
+            public double LeftRecangle { get; private set; }
+            public double RightRecangle { get; private set; }
+            public double MiddleRecangle { get; private set; }
+            public double Trapezoidal { get; private set; }
+            public double Simpsons { get; private set; }
+        }
+
+        public static Integral Integrate(Func<double, double> function, double left, double right, int segmentNumber)
+        {
+            if (left >= right || segmentNumber < 1 || !left.IsNumber() || !right.IsNumber())
+            {
+                throw new ArgumentException();
+            }
+            
+            var firstValue = 0d;
+            var lastValue = 0d;
+            var trimmedSum = 0d;
+            var middleSum = 0d;
+
+            var segmentLength = (right - left) / segmentNumber;
+
+            for (var i = 0; i < segmentNumber; ++i)
+            {
+                var value = function(left + segmentLength * i);
+                var middleValue = function(left + segmentLength * i + segmentLength / 2);
+
+                if (i == 0) 
+                {
+                    firstValue = value;
+                }
+                else
+                {
+                    trimmedSum += value;
+                }
+
+                middleSum += middleValue;
+            }
+
+            lastValue = function(right);
+
+            var leftRectangle = segmentLength * (firstValue + trimmedSum);
+            var rightRectangle = segmentLength * (lastValue + trimmedSum);
+            var middleRectangle = segmentLength * middleSum;
+            var trapezoidal = segmentLength * (0.5d * (firstValue + lastValue) + trimmedSum);
+            var simpsons = segmentLength / 6 * (4 * middleSum + 2 * trimmedSum + firstValue + lastValue);
+
+            return new Integral(leftRectangle, rightRectangle, middleRectangle, trapezoidal, simpsons);
+        }
+    }
+}
