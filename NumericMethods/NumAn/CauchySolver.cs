9251c14361878aaa5f4e417bdff6e58ca417dedf
mxprshn <mxprshn@gmail.com>
2020-12-06 16:59:09 +0300

    Add project files

---
+++ NumericMethods/NumAn/CauchySolver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumAn
+{
+    public class CauchySolver
+    {
+        public CauchySolver(Func<double, double, double> derivative, double targetX, double targetY)
+        {
+
+        }
+
+        public List<(double x, double y)> Taylor(double segmentLength, int segmentNumber)
+        {
+
+        }
+
+        public List<(double x, double y)> Adams(double segmentLength, int segmentNumber)
+        {
+
+        }
+
+        public List<(double x, double y)> RungeKutta(double segmentLength, int segmentNumber)
+        {
+
+        }
+
+        public List<(double x, double y)> Euler(double segmentLength, int segmentNumber)
+        {
+
+        }
+
+        public List<(double x, double y)> EulerI(double segmentLength, int segmentNumber)
+        {
+
+        }
+
+
+
+        public List<(double x, double y)> EulerII(double segmentLength, int segmentNumber)
+        {
+
+        }
+    }
+}
