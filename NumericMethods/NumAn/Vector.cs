42af516802748657e59765e647063608a8dd0d60
mxprshn <mxprshn@gmail.com>
2021-02-21 18:23:20 +0300

    Add matrix and vector classes

---
+++ NumericMethods/NumAn/Vector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumAn
+{
+    public class Vector : Matrix
+    {
+        public Vector(double[] elements) : base(ToTwoDimensionalArray(elements)) { }
+
+        private static double[,] ToTwoDimensionalArray(double[] oneDimensional)
+        {
+            var result = new double[oneDimensional.Length, 1];
+
+            for (var i = 0; i < oneDimensional.Length; ++i)
+            {
+                result[i, 1] = oneDimensional[i];
+            }
+
+            return result;
+        }
+    }
+}
