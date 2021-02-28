a9f9d0312f80f47107a0e31d58f0f6bada42d6d5
mxprshn <mxprshn@gmail.com>
2021-02-28 13:03:30 +0300

    Add gauss solver class

---
+++ NumericMethods/NumAn/GaussSystemSolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NumAn
+{
+    namespace SystemSolving
+    {
+        public class GaussSystemSolver
+        {
+            public static Vector Solve(Matrix systemMatrix, Vector constantVector)
+            {
+                if (systemMatrix.Height != systemMatrix.Width)
+                {
+                    throw new ArgumentException("System matrix must be square");
+                }
+
+                if (systemMatrix.Height != constantVector.Height)
+                {
+                    throw new ArgumentException("Constant vector must have length equal to system matrix size");
+                }
+
+
+            }
+        }
+    }
+
+}
